e75635617b6cde291156f309d4a7d902fa09886f
Michael <mokelder4@gmail.com>
2022-10-04 16:35:48 -0400

    third commit

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,47 +1,63 @@
+/**
+ * @author Michael Elder
+ * Assignment 5.1 HashMap
+ * Dictionary class creates the load dictionary method, search dictionary method, and print dictionary method
+ */
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.FileNotFoundException;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Scanner;
 
 public class Dictionary{
 	private HashMap<Integer, String> myMap;
 	
+	/**
+	 * Constructor creates a new HashMap
+	 */
 	public Dictionary() {
-		HashMap<Integer, String> myMap = new HashMap<Integer, String>();
+		myMap = new HashMap<Integer, String>();
 	}//end constructor
 	
-	public HashMap<Integer, String> loadDictionary(File xfile) throws NumberFormatException, IOException {
+	/**
+	 * Load dictionary method takes a file as a parameter and loads the file into the system. The method reads the
+	 * file while there are lines in the document and puts it into an array and then hashes the key. 
+	 * 
+	 * @param xfile
+	 * @throws NumberFormatException
+	 * @throws IOException
+	 */
+	public void loadDictionary(File xfile) throws NumberFormatException, IOException{
 
-		HashMap<Integer, String> myMap = new HashMap<Integer, String>();
-			xfile = new File("C:\\Users\\mokel\\Downloads\\DictionaryWordValuePairs.txt");
-			BufferedReader br = null;
-			br = new BufferedReader(new FileReader(xfile));
-			String line = null;
+			BufferedReader br = null;//creates a buffered reader
+			br = new BufferedReader(new FileReader(xfile));//buffered reader loads in the document which is the parameter
+			String line = br.readLine();//creating a string variable line that refers to the line that is read by the file
 	
-			while ((line = br.readLine()) != null) 
+			while (line != null) 
 			{ 
-			String []array = line.split(":");
-			Integer key = Integer.parseInt(array[0].trim());//get the key 
-			String value = array[1].trim();//get the value
-			if(!key.equals("") && !value.equals(""))
-				myMap.put(key, value);
+			String[] array = line.split("\t");//splits the array with a tab
+			String key = array[0];//get the key
+			String value = array[1];//get the value
+				myMap.put(key.hashCode(), value);//puts the hashcode created by the key and the value into an array
+			line = br.readLine();//reads the next line
 			}
-			br.close();
-		return myMap;	
+			br.close();//closes the file reader
+		
 		
 	}//end loadDictionary
 	
-	public void searchDictionary(String search) {
-		search.hashCode();
-		myMap.get(search);
+	/**
+	 * Search dictionary method returns the value of the search hashcode.
+	 */
+	public String searchDictionary(String search) {
+		return myMap.get(search.hashCode());
 		
 	}//end searchDirectory
 	
+	/**
+	 * Print dictionary prints the HashMap by iterating through all the lines.
+	 */
 	public void printDictionary() {
 		for(Entry<Integer, String> entry : myMap.entrySet())
 			System.out.println("Key: " + entry.getKey() + " , Value: " + entry.getValue());
--- src/Driver.java
+++ src/Driver.java
@@ -1,22 +1,35 @@
-
-  
+/**
+ * @author Michael Elder
+ * Assignment 5.1 HashMap
+ * Driver class loads a file into the loadDictionary method. It then searches the dictionary for 5 words and then
+ * printDictionary prints out the entire dictionary with the hashcode and the value which is the definition.
+ */
 import java.io.*;
-import java.util.*;
   
 class Driver {
     
-	public static void main(String[] args)
+	public static void main(String[] args) throws NumberFormatException, IOException
     {
 		
 		File xfile = new File("C:\\Users\\mokel\\Downloads\\DictionaryWordValuePairs.txt");
+		Dictionary myMap = new Dictionary();
 		myMap.loadDictionary(xfile);
-		myMap.searchDictionary("carl");
-		myMap.searchDictionary("gyve");
-		myMap.searchDictionary("cicisbeo");
-		myMap.searchDictionary("virtue");
-		myMap.searchDictionary("plight");
+		System.out.println("appetency");
+		System.out.println(myMap.searchDictionary("appetency"));
+		System.out.println("appetency".hashCode());
+		System.out.println("mooncalf");
+		System.out.println(myMap.searchDictionary("mooncalf"));
+		System.out.println("mooncalf".hashCode());
+		System.out.println("damsel");
+		System.out.println(myMap.searchDictionary("damsel"));
+		System.out.println("damsel".hashCode());
+		System.out.println("egad");
+		System.out.println(myMap.searchDictionary("egad"));
+		System.out.println("egad".hashCode());
+		System.out.println("repulsive");
+		System.out.println(myMap.searchDictionary("repulsive"));
+		System.out.println("repulsive".hashCode());
 		myMap.printDictionary();
-		
     }//end main
     
 }//end class
